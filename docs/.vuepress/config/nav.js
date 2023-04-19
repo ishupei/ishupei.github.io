c7603eadef75b4ea53bd976551e1183f1f6b29f1
hupei <ishupei@qq.com>
2023-04-19 20:26:27 +0800

    version 1.2

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,5 +1,6 @@
 module.exports = [
-  { text: "Home", link: "/" },
+  { text: "首页", link: "/" },
+  { text: "关于", link: "/about/" },
   { text: "接单流程", link: "/flow/" },
   { text: "环境安装", link: "/env/" },
   { text: "视频合集", link: "/video/" },
