83e8acd0e3476c2ea064aff177ef79f021793e80
hupei <hupei@kehaoinfo.com>
2025-04-23 20:14:06 +0800

    v1.9

--- docs/.vuepress/config/plugin.js
+++ docs/.vuepress/config/plugin.js
@@ -2,13 +2,5 @@ module.exports = {
   "@vuepress/active-header-links": {},
   "@vuepress/back-to-top": {},
   "vuepress-plugin-auto-sidebar": {},
-  "vuepress-plugin-comment":{
-    choosen: 'valine', 
-    // options选项中的所有参数，会传给Valine的配置
-    options: {
-      el: '#valine-vuepress-comment',
-      appId: '9waUs7K5h7gcZnzryBAo90KM-gzGzoHsz',
-      appKey: 'u4quYy39ho5MiyddsFPO4tgM'
-    }
-  }
+  "@vuepress/plugin-reading-time": {},
 };
