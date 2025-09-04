deb7a3429f415a7b8e84fa0b5e456e192a45bd3d
hupei <ishupei@qq.com>
2025-09-04 19:30:36 +0800

    fix：fix sidebar

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -2,16 +2,86 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
   "/env/": "structure",
+  "/video/": "structure",
   "/flow/": "structure",
   "/": [
     {
-      text: "Java",
-      icon: "java",
-      prefix: "/java/",  // 现在路径基于 src/ 目录
+      text: "JAVA",
+      icon: "mug-hot",
+      prefix: "java/",
+      children: [
+        {
+          text: "语言基础",
+          prefix: "basic/",
+          icon: "cubes-stacked",
+          collapsible: true,
+          children: [
+            "basic",
+            "exception",
+            "generics",
+            "reflection",
+            "annotation",
+            "io",
+            
+          ],
+        },
+        {
+          text: "并发编程",
+          prefix: "juc/",
+          icon: "water",
+          collapsible: true,
+          children: [
+          ],
+        },
+        {
+          text: "JVM详解",
+          prefix: "jvm/",
+          icon: "smoking",
+          collapsible: true,
+          children: [
+          ],
+        },
+      ],
+    },
+    {
+      text: "计算机基础",
+      icon: "computer",
+      prefix: "cs/",
       collapsible: true,
       children: [
-        "basic",    // 对应 src/java/basic.md
-      ]
-    }
-  ]
+        {
+          text: "数据结构",
+          prefix: "data-structure/",
+          icon: "smoking",
+          collapsible: true,
+          children: [
+            "introduction",
+            "linear-list",
+            "stack-queue",
+            "string",
+            "tree",
+            "map",
+            "search-sort",
+          ],
+        },
+        {
+          text: "计算机网络",
+          prefix: "network/",
+          icon: "smoking",
+          collapsible: true,
+          children: [
+          ],
+        },
+        {
+          text: "操作系统",
+          prefix: "operating-system/",
+          icon: "smoking",
+          collapsible: true,
+          children: [
+          ],
+        },
+      ],
+    },
+  ],
 });
+
